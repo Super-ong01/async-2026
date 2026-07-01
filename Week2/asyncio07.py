a5516aedbe50fd9edfe6bd58b45ed2819ec3f734
Super-ong01 <chanasornong2548@gmail.com>
2026-07-01 11:12:10 +0700

    no

--- Week2/asyncio07.py
+++ Week2/asyncio07.py
@@ -1,2 +1,26 @@
 # Program 7: Dual Tasks Concurrency
 # Concept: Scheduling two distinct tasks concurrently and awaiting them individually without gather.
+import asyncio
+from time import time,ctime
+
+async def cook_spaghtti(customer):
+    print(f"{ctime()} | Cooking spaghetti for {customer}...")
+    await asyncio.sleep(1)  # Simulate a delay of 1 second
+    print(f"{ctime()} | Finished cooking spaghetti for {customer}.")
+    
+async def main():
+    start = time()
+    
+    # Create a task for cooking spaghetti for Alice
+    task1 = asyncio.create_task(cook_spaghtti("Alice")) #สร้าง task1 ขึ้นมาเพื่อให้ทำงานแบบ Async
+    
+    # Create a task for cooking spaghetti for Bob
+    task2 = asyncio.create_task(cook_spaghtti("Bob"))
+    
+    # Wait for both tasks to complete
+    await task1  #สำคัญ เพราะทำให้เป็น Async
+    await task2
+    print(f"Total Time: {time() - start:.2f} seconds")
+    
+if __name__ == "__main__":
+    asyncio.run(main())  # Run the main coroutine using the event loop
